f76dfb506328c091753110a5536b05ddaaea4629
shota.web3 <shota.web.2355@gmail.com>
2023-12-03 18:33:12 +0900

    first commit

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsEmpty, IsNotEmpty, IsString, MinLength } from "class-validator";
+export class AuthDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  password: string;
+}
